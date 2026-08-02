ff0ed8150f8bdfc89b9013461d19fe4b1ef4cb57
dwightmayer836-pixel <dwightmayer836@gmail.com>
2026-08-02 00:50:18 -0400

    Add files via upload

--- RUBY ML KIT/core/rusty_tensor.rb
+++ RUBY ML KIT/core/rusty_tensor.rb
@@ -38,10 +38,101 @@ module RustOperations
 
   end
 
-  def add(other)
+  def add_rust(other)
     # need to pad other / self tensor
 
+    # other = other.to_tensor if other.is_a?(Matrix)
+    # other = wrap_as_tensor(other) unless other.is_a?(Tensor)
+
+    result_shape = self.class.broadcast_shape(@shape, other.shape)
+    self_expanded = broadcast_to(result_shape)
+    other_expanded = other.broadcast_to(result_shape)
+
+    result = RustyTensor.elementwise_add(self_expanded.data, other_expanded.data)
+    Tensor.new(result, result_shape)
+    
   end
+
+  def scalar_multiply(scalar)
+    result = RustyTensor.scalar_multiply(self.data, scalar)
+    Tensor.new(result, self.shape)
+  end
+
+  def scalar_divide(scalar)
+    raise ZeroDivisionError unless scalar != 0
+
+    result = RustyTensor.scalar_divide(self.data, scalar)
+    Tensor.new(result, self.shape)
+  end
+
+  def hadamard_multiply(other)
+    #other = wrap_as_tensor(other) unless other.is_a?(Tensor)
+    
+    other = other.to_tensor if other.is_a?(Matrix)
+    other = wrap_as_tensor(other) unless other.is_a?(Tensor)
+
+    result_shape = self.class.broadcast_shape(@shape, other.shape)
+    self_expanded = broadcast_to(result_shape)
+    other_expanded = other.broadcast_to(result_shape)        
+    
+   
+    result = RustyTensor.elementwise_multiply(self_expanded.data, other_expanded.data)
+    Tensor.new(result, self.shape)
+  end
+
+  def sigmoid_rust
+    source = contiguous? ? self : materialize
+    result = RustyTensor.sigmoid(source.data)
+    Tensor.new(result, source.shape)
+  end
+
+  def relu_rust
+    source = contiguous? ? self : materialize
+    result = RustyTensor.relu(source.data)
+    Tensor.new(result, source.shape)
+  end
+
+  def tanh_rust
+    source = contiguous? ? self : materialize
+    result = RustyTensor.tanh(source.data)
+    Tensor.new(result, source.shape)
+  end
+
+  def sigmoid_derivative_rust
+    source = contiguous? ? self : materialize
+    result = RustyTensor.tanh_derivative(source.data)
+    Tensor.new(result, source.shape)
+
+  end
+
+  def relu_derivative_rust
+    source = contiguous? ? self : materialize
+    result = RustyTensor.relu_derivative(source.data)
+    Tensor.new(result, source.shape)
+  end
+
+  def tanh_derivative_rust
+    source = contiguous? ? self : materialize
+    result = RustyTensor.tanh_derivative(source.data)
+    Tensor.new(result, source.shape)
+
+  end
+
+  def elu_rust(alpha:1)
+    source = contiguous? ? self : materialize
+    result = RustyTensor.elu(source.data,alpha)
+    Tensor.new(result, source.shape)
+  end
+
+  def elu_derivative_rust(alpha:1)
+    source = contiguous? ? self : materialize
+    result = RustyTensor.elu_derivative(source.data,alpha)
+    Tensor.new(result, source.shape)
+  end
+
+
+
+
 
 
 end
